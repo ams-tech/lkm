71ab0f70ae28e0d35587fc6510ae440456b132b2
ams-tech <ams-tech@users.noreply.github.com>
2015-06-16 04:12:51 +0000

    Phy is complete

--- modules/meters/common/inc/meter_phy.h
+++ modules/meters/common/inc/meter_phy.h
@@ -15,7 +15,12 @@ typedef enum gpio_function_X
 	GPIO_ALT_FUNC_2 = 6,
 	GPIO_ALT_FUNC_3 = 7,
 	GPIO_ALT_FUNC_4 = 3,
-	GPIO_ALT_FUNC_5 = 2
+	GPIO_ALT_FUNC_5 = 2,
 }gpio_function_t;
+
+meter_error_t clear_gpio_pin(u32 gpio_num);
+meter_error_t set_gpio_pin(u32 gpio_num);
+meter_error_t get_gpio_pin(u32 gpio_num, bool * value);
+
 
 #endif
