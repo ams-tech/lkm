c83fa0964a3c6ad81ba5fa02555f4f85d341700c
ams-tech <ams-tech@users.noreply.github.com>
2015-09-07 15:50:48 -0600

    Meter is functional, but GPIOs have to be driven WAY too slow

--- modules/meters/chips/mcp3301.c
+++ modules/meters/chips/mcp3301.c
@@ -3,20 +3,25 @@
 #include "meter_phy.h"
 #include <linux/delay.h>
 #include "meter.h"
+#include <linux/kernel.h>
 
-#define CS_DISABLE_TIME_NS	580
-#define CS_ENABLE_HOLD_TIME_NS	100
-#define CLOCK_HALF_PERIOD_NS	300
+#define METER_TIMING_FUDGE	1
+
+#define CS_DISABLE_TIME_NS	(580 * METER_TIMING_FUDGE)
+#define CS_ENABLE_HOLD_TIME_NS	(100 * METER_TIMING_FUDGE)
+#define CLOCK_HALF_PERIOD_NS	(300)
 
 static inline bool read_bit(mcp3301_t * chip)
 {
 	bool retval;
 
+	get_gpio_pin(chip->gpio.data, &retval);
 	set_gpio_pin(chip->gpio.clk);
-        ndelay(CLOCK_HALF_PERIOD_NS);
+        mdelay(CLOCK_HALF_PERIOD_NS);
 	get_gpio_pin(chip->gpio.data, &retval);
         clear_gpio_pin(chip->gpio.clk);
-        ndelay(CLOCK_HALF_PERIOD_NS);
+        mdelay(CLOCK_HALF_PERIOD_NS);
+	
 
 	return retval;
 }
@@ -28,6 +33,8 @@ static u16 read_voltage(mcp3301_t * chip)
 
 	mutex_lock(&(chip->lock));
 
+	printk(KERN_DEBUG "Initializing meter GPIO\n");
+
 	set_gpio_function(chip->gpio.data, GPIO_INPUT);
 
 	clear_gpio_pin(chip->gpio.clk);
@@ -36,17 +43,21 @@ static u16 read_voltage(mcp3301_t * chip)
 	set_gpio_pin(chip->gpio.enable);
 	set_gpio_function(chip->gpio.enable, GPIO_OUTPUT);
 
-	ndelay(CS_DISABLE_TIME_NS);
+	printk(KERN_DEBUG "Init complete, lowering chip enable\n");
+
+	udelay(CS_DISABLE_TIME_NS);
 
 	clear_gpio_pin(chip->gpio.enable);
 
-	ndelay(CS_ENABLE_HOLD_TIME_NS);
+	udelay(CS_ENABLE_HOLD_TIME_NS);
 
-	/* fCLK = 800kHz.  Half-period is 625 ns */
+	printk(KERN_DEBUG "Reading two blank bits\n");
 
 	/* Two blank bits */
 	read_bit(chip);
 	read_bit(chip);
+
+	printk(KERN_DEBUG "Reading NULL byte\n");
 
 	/* a null byte, but always 0*/
 	if(read_bit(chip))
@@ -57,11 +68,16 @@ static u16 read_voltage(mcp3301_t * chip)
 
 	for(x = 0; x < RESULT_BITS; x++)
 	{
+		printk(KERN_DEBUG "Reading bit number %d\n", x);
 		if(read_bit(chip))
 			retval |= 1 << (RESULT_BITS - 1 - x);
 	}
 
+	printk(KERN_DEBUG "Read complete, setting chip select\n");
+
 	set_gpio_pin(chip->gpio.enable);
+
+	printk(KERN_DEBUG "Freeing the mutex and exiting MCP3301 read\n");
 
 exit:
 	mutex_unlock(&(chip->lock));
